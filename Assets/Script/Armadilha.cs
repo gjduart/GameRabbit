9c89a516dd9fbcb84e181f903ff4ceb5c313ac4a
Gabriel Duarte <39168030+gjduart@users.noreply.github.com>
2020-07-29 15:21:04 -0300

    0.0.2.32

--- Assets/Script/Armadilha.cs
+++ Assets/Script/Armadilha.cs
@@ -4,22 +4,5 @@ using UnityEngine;
 
 public class Armadilha : MonoBehaviour
 {
-     private Rigidbody2D rb;
-    private SpriteRenderer sr;
-    private CircleCollider2D circle;
-    private BoxCollider2D box;
-    void Start()
-    {
-        sr = GetComponent<SpriteRenderer>();
-        circle = GetComponent<CircleCollider2D>();
-        box = GetComponent<BoxCollider2D>();
-    }
-    private void OnCollisionEnter2D(Collision2D col) {
-        if(col.)
-    }
-    private void OnTriggerEnter2D(Collider2D col) {
-        if (col.gameObject.CompareTag("Player")){
-            rb.bodyType = RigidbodyType2D.Dynamic;
-        }    
-    }
+
 }
---
+++ Assets/Script/Destrutivo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destrutivo : MonoBehaviour
+{
+    public Rigidbody2D rg;
+    public float time = 1f;
+    void Start()
+    {
+        
+    }
+
+private void OnCollisionEnter2D(Collision2D colisor) {
+    
+    if (colisor.gameObject.tag.Equals("Player"))
+
+         rg.bodyType = RigidbodyType2D.Dynamic;
+
+     }
+        
+}
+
+
