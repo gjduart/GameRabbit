6645ac255f5284af339731843b3c81b6ad345515
Gabriel Duarte <39168030+gjduart@users.noreply.github.com>
2020-07-31 20:27:36 -0300

    0.3.2.5

---
+++ Assets/Script/Cenario/Bolhas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolhas : MonoBehaviour
+{
+    private float time = 0.0f;
+    public float timer;
+    public bool inwater = true;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+      //  GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+        if (!inwater){
+            
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.CompareTag("agua")){
+            Destroy(gameObject);
+        }
+    }
+}
